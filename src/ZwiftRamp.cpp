5a04f2223aae7c65b2222d78425fef9563a34796
vincent golle <v.golle@isispace.nl>
2021-05-10 14:58:48 +0200

    Added first file

---
+++ src/ZwiftRamp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ZwiftRamp.cpp
+// Author      : Vincent Golle
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
